e5b320f079ab8f4ecf5b261b51f7ae5e167994ec
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2024-08-09 23:53:34 +0530

    ""

--- BMI/bmi.js
+++ BMI/bmi.js
@@ -3,7 +3,8 @@ form.addEventListener('submit',function(e){
     e.preventDefault()
     const height = parseInt(document.querySelector('#height').value);
     const weight = parseInt(document.querySelector('#weight').value);
-    const result = document.querySelector('.result')
+    const result = document.querySelector('.result');
+    const info = document.querySelector('.info');
 
     if (height === '' || height < 0 || isNaN(height)){
             result.innerHTML = `Please give a valid height ${height}`
@@ -15,5 +16,14 @@ form.addEventListener('submit',function(e){
         const bmi = (weight / ((height * height) / 10000)).toFixed(2);
 
         result.innerHTML = `<span>${bmi}</span>`;
+        if(bmi < 18.5){
+            info.innerHTML = `<h1>Under Weight</h1>`;
+        }
+        else if(bmi >= 18.5 && bmi <= 24.5){
+            info.innerHTML = `<h1>Normal Weight</h1>`;
+        }
+        else{
+            info.innerHTML = `<h1>Over Weight</h1>`;
+        }
     }
 })
