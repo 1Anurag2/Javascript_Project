72ecc3d86876f30e8dc7fbc2ff95a32bb70d4f5c
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2024-08-10 00:02:15 +0530

    ""

--- BMI/bmi.js
+++ BMI/bmi.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('.form');
 form.addEventListener('submit',function(e){
     e.preventDefault()
-    const height = parseInt(document.querySelector('#height').value);
+    const height = parseInt(document.getElementById('height').value);
     const weight = parseInt(document.querySelector('#weight').value);
     const result = document.querySelector('.result');
     const info = document.querySelector('.info');
